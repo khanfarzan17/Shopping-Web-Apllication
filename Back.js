c1522e919d07e2764c1096ea29ccf83d4fc21fbd
khanfarzan17 <116535467+khanfarzan17@users.noreply.github.com>
2023-08-27 22:47:52 +0530

    Add files via upload

---
+++ Back.js
@@ -0,0 +1,16 @@
+function darkMode() {
+    let element = document.body;
+    let content = document.getElementById("DarkModetext");
+    element.className = "dark-mode";
+    content.innerText = "Dark Mode is ON";
+}
+function lightMode() {
+    let element = document.body;
+    let content = document.getElementById("DarkModetext");
+    element.className = "light-mode";
+    content.innerText = "Dark Mode is OFF";
+}
+
+function googleTranslateElementInit() {
+    new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
+  }
